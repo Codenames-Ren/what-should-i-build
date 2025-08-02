56c54887b6b0f4af3c5d76779f38ecd56a94cb8d
Codenames-Ren <renmizuki38@gmail.com>
2025-08-02 23:07:39 +0700

    add data.ts

---
+++ src/lib/data.ts
@@ -0,0 +1,45 @@
+export type Category = {
+  name: string;
+  ideas: string[];
+};
+
+export const categories: Category[] = [
+  {
+    name: "Web Projects",
+    ideas: [
+      "Todo app with dark mode",
+      "Personal portfolio with animation",
+      "Recipe sharing site with search",
+      "Web based game",
+      "CRUD with Golang",
+    ],
+  },
+  {
+    name: "Mobile Projects",
+    ideas: [
+      "Simple note-taking app",
+      "Workout tracker",
+      "Offline journal app",
+      "Money notes app",
+    ],
+  },
+  {
+    name: "Game Development Projects",
+    ideas: [
+      "Try Unity and learn Unity Fundamentals",
+      "Trying C# and make simple games",
+      "Simple games with pygames",
+      "Web based games",
+    ],
+  },
+  {
+    name: "Healing",
+    ideas: [
+      "Play Honkai Star Rail",
+      "Play Dragon Nest M Classic",
+      "Play Wuthering Waves",
+      "Turu seharian hehe 😁",
+      "Jajan, makan enak 😆",
+    ],
+  },
+];
